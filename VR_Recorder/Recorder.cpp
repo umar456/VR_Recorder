5a5ec33c630902b9f6459ae526f45818a4e33687
Umar Arshad <umar@arrayfire.com>
2017-03-11 17:31:49 -0500

    Updated OpenCV and OpenVR packages to point to NuGet

--- VR_Recorder/Recorder.cpp
+++ VR_Recorder/Recorder.cpp
@@ -1,15 +1,12 @@
 // Copyright 2017 Umar Arshad
 
+#include <opencv2/opencv.hpp>
 #include <openvr.h>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <experimental/filesystem>
 
 #include <array>
 #include <cstdio>
 #include <cstdlib>
+#include <experimental/filesystem>
 #include <string>
 #include <vector>
 
